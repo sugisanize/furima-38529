c1f76a38d6a60237d07d8979fb13b69c6d88c918
sugisanize <sugitaku0818@yahoo.co.jp>
2022-12-30 22:32:55 +0900

    FurimasController削除、ItemsController集約

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 2022_12_29_142452) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "furimas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "content", null: false
