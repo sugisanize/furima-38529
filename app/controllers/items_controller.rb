85683f71bb977b28bf5327f0eeacff039eb9f8e9
sugisanize <sugitaku0818@yahoo.co.jp>
2023-01-07 18:59:28 +0900

    不要コード削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return unless user_signed_in? && current_user.id == @item.user_id
+    return unless current_user.id == @item.user_id
 
     @item.destroy
     redirect_to root_path
