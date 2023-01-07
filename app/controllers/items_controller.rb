1e3d6c1b048abb4af0a89f8e59dc307a43bd4024
sugisanize <sugitaku0818@yahoo.co.jp>
2023-01-07 18:22:56 +0900

    itemsコントローラーのdestroyアクションに条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,6 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    return unless user_signed_in? && current_user.id == @item.user_id
+
     @item.destroy
     redirect_to root_path
   end
